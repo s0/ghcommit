5b39eca7e73598386021e9a9d0d798a81010ac23
Sam Lanning <sam@samlanning.com>
2024-07-27 17:57:26 +0100

    Add tests for using commits / tags as base

--- src/test/integration/node.test.ts
+++ src/test/integration/node.test.ts
@@ -52,6 +52,62 @@ describe("node", () => {
         });
       }
     });
+
+    it("can commit using tag as a base", async () => {
+      const branch = `${TEST_BRANCH_PREFIX}-tag-base`;
+      branches.push(branch);
+      const contents = Buffer.alloc(1024, "Hello, world!");
+
+      await commitFilesFromBuffers({
+        octokit,
+        ...REPO,
+        branch,
+        base: {
+          tag: "v0.1.0",
+        },
+        message: {
+          headline: "Test commit",
+          body: "This is a test commit",
+        },
+        fileChanges: {
+          additions: [
+            {
+              path: `foo.txt`,
+              contents,
+            },
+          ],
+        },
+        log,
+      });
+    });
+
+    it("can commit using commit as a base", async () => {
+      const branch = `${TEST_BRANCH_PREFIX}-commit-base`;
+      branches.push(branch);
+      const contents = Buffer.alloc(1024, "Hello, world!");
+
+      await commitFilesFromBuffers({
+        octokit,
+        ...REPO,
+        branch,
+        base: {
+          commit: "fce2760017eab6d85388ed5cfdfac171559d80b3",
+        },
+        message: {
+          headline: "Test commit",
+          body: "This is a test commit",
+        },
+        fileChanges: {
+          additions: [
+            {
+              path: `foo.txt`,
+              contents,
+            },
+          ],
+        },
+        log,
+      });
+    });
   });
 
   afterAll(async () => {
