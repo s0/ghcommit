1ee8ba4f9537ffabf41d5a7dff295194d1b77c4b
Sam Lanning <sam@samlanning.com>
2024-07-27 17:59:03 +0100

    Add isomorphic git to project, and allow different bases (#8)
    * Add isomorphic-git to the project
    
    This required the typescript module resolution
    mode.
    
    * Allow for different types of bases
    
    Allow for tags, either the same or another branch,
    or a commit hash to be used as a base.
    
    * Add tests for using commits / tags as base
    
    * Fix bug using tag as base

--- src/core.ts
+++ src/core.ts
@@ -8,12 +8,26 @@ import {
   getRepositoryMetadata,
   GitHubClient,
 } from "./github/graphql/queries.js";
-import type { CreateCommitOnBranchMutationVariables } from "./github/graphql/generated/operations.js";
+import type {
+  CreateCommitOnBranchMutationVariables,
+  GetRepositoryMetadataQuery,
+} from "./github/graphql/generated/operations.js";
 import type { Logger } from "./logging.js";
 
 export type CommitFilesResult = {
   refId: string | null;
 };
+
+export type GitBase =
+  | {
+      branch: string;
+    }
+  | {
+      tag: string;
+    }
+  | {
+      commit: string;
+    };
 
 export type CommitFilesFromBase64Args = {
   octokit: GitHubClient;
@@ -21,9 +35,9 @@ export type CommitFilesFromBase64Args = {
   repository: string;
   branch: string;
   /**
-   * The current commit that the target branch is at
+   * The current branch, tag or commit that the new branch should be based on.
    */
-  baseBranch: string;
+  base: GitBase;
   /**
    * The commit message
    */
@@ -32,18 +46,47 @@ export type CommitFilesFromBase64Args = {
   log?: Logger;
 };
 
+const getBaseRef = (base: GitBase): string => {
+  if ("branch" in base) {
+    return `refs/heads/${base.branch}`;
+  } else if ("tag" in base) {
+    return `refs/tags/${base.tag}`;
+  } else {
+    return "HEAD";
+  }
+};
+
+const getOidFromRef = (
+  base: GitBase,
+  ref: (GetRepositoryMetadataQuery["repository"] & Record<never, never>)["ref"],
+) => {
+  if ("commit" in base) {
+    return base.commit;
+  }
+
+  if (!ref?.target) {
+    throw new Error(`Could not determine oid from ref: ${JSON.stringify(ref)}`);
+  }
+
+  if ("target" in ref.target) {
+    return ref.target.target.oid;
+  }
+
+  return ref.target.oid;
+};
+
 export const commitFilesFromBase64 = async ({
   octokit,
   owner,
   repository,
   branch,
-  baseBranch,
+  base,
   message,
   fileChanges,
   log,
 }: CommitFilesFromBase64Args): Promise<CommitFilesResult> => {
   const repositoryNameWithOwner = `${owner}/${repository}`;
-  const baseRef = `refs/heads/${baseBranch}`;
+  const baseRef = getBaseRef(base);
 
   log?.debug(`Getting repo info ${repositoryNameWithOwner}`);
   const info = await getRepositoryMetadata(octokit, {
@@ -57,36 +100,57 @@ export const commitFilesFromBase64 = async ({
     throw new Error(`Repository ${repositoryNameWithOwner} not found`);
   }
 
-  const oid = info.ref?.target?.oid;
-
-  if (!info) {
+  if (!info.ref) {
     throw new Error(`Ref ${baseRef} not found`);
   }
 
-  log?.debug(`Creating branch ${branch} from commit ${oid}}`);
-  const refId = await createRefMutation(octokit, {
-    input: {
-      repositoryId: info.id,
-      name: `refs/heads/${branch}`,
-      oid,
-    },
-  });
+  const repositoryId = info.id;
+  /**
+   * The commit oid to base the new commit on.
+   *
+   * Used both to create / update the new branch (if necessary),
+   * and th ensure no changes have been made as we push the new commit.
+   */
+  const baseOid = getOidFromRef(base, info.ref);
 
-  log?.debug(`Created branch with refId ${JSON.stringify(refId, null, 2)}`);
+  let refId: string;
 
-  const refIdStr = refId.createRef?.ref?.id;
+  if ("branch" in base && base.branch === branch) {
+    log?.debug(`Committing to the same branch as base: ${branch} (${baseOid})`);
+    // Get existing branch refId
+    refId = info.ref.id;
+  } else {
+    // Create branch as not committing to same branch
+    // TODO: detect if branch already exists, and overwrite if so
+    log?.debug(`Creating branch ${branch} from commit ${baseOid}}`);
+    const refIdCreation = await createRefMutation(octokit, {
+      input: {
+        repositoryId,
+        name: `refs/heads/${branch}`,
+        oid: baseOid,
+      },
+    });
 
-  if (!refIdStr) {
-    throw new Error(`Failed to create branch ${branch}`);
+    log?.debug(
+      `Created branch with refId ${JSON.stringify(refIdCreation, null, 2)}`,
+    );
+
+    const refIdStr = refIdCreation.createRef?.ref?.id;
+
+    if (!refIdStr) {
+      throw new Error(`Failed to create branch ${branch}`);
+    }
+
+    refId = refIdStr;
   }
 
   await log?.debug(`Creating commit on branch ${branch}`);
   const createCommitMutation: CreateCommitOnBranchMutationVariables = {
     input: {
       branch: {
-        id: refIdStr,
+        id: refId,
       },
-      expectedHeadOid: oid,
+      expectedHeadOid: baseOid,
       message,
       fileChanges,
     },
