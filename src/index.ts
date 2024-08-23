4e931fb2970c3e01c86ac0dcb5ddf1e5e446e749
Sam Lanning <sam@samlanning.com>
2024-08-23 19:48:03 +0100

    Introduce commitChangesFromRepo

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export * as queries from "./github/graphql/queries.js";
+export { commitFilesFromBase64 } from "./core.js";
+export { commitChangesFromRepo } from "./git.js";
 export { commitFilesFromDirectory } from "./fs.js";
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts", "src/core.ts", "src/fs.ts", "src/node.ts"],
+  entry: [
+    "src/index.ts",
+    "src/core.ts",
+    "src/git.ts",
+    "src/fs.ts",
+    "src/node.ts",
+  ],
   format: ["cjs", "esm"],
   dts: true,
 });
